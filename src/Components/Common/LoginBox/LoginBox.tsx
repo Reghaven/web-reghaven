e9d27bce1f3295c9f0eae6538eaff03dcd7482cc
Tobias Kavsek <tovuko@hotmail.de>
2022-01-26 23:28:25 +0100

    build switch

--- src/Components/Common/LoginBox/LoginBox.tsx
+++ src/Components/Common/LoginBox/LoginBox.tsx
@@ -1,7 +1,79 @@
-import React from 'react';
-import { Typography, Box, Paper, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Typography, Box, Paper, TextField, Button, Grid } from '@mui/material';
 
 export function LoginBox() {
+	enum Modes {
+		LOGIN,
+		REGISTER,
+	}
+
+	const [mode, setMode] = useState<Modes>(Modes.LOGIN);
+
+	const Login = (
+		<Grid
+			container
+			direction={'column'}
+			alignItems={'center'}
+			justifyContent={'center'}
+		>
+			<Typography variant={'h5'} sx={{ marginBottom: 2 }}>
+				Login
+			</Typography>
+			<TextField
+				variant={'outlined'}
+				label={'Username'}
+				sx={{ width: '100%', marginBottom: 2 }}
+			/>
+			<TextField
+				type={'password'}
+				variant={'outlined'}
+				label={'Password'}
+				sx={{ width: '100%', marginBottom: 2 }}
+			/>
+			<Button variant={'contained'} sx={{ marginBottom: 1 }}>
+				Login
+			</Button>
+			<Button variant={'text'} onClick={() => setMode(Modes.REGISTER)}>
+				new account
+			</Button>
+		</Grid>
+	);
+
+	const Register = (
+		<Grid
+			container
+			direction={'column'}
+			alignItems={'center'}
+			justifyContent={'center'}
+		>
+			<Typography variant={'h5'} sx={{ marginBottom: 2 }}>
+				Create new account
+			</Typography>
+			<TextField
+				variant={'outlined'}
+				label={'Username'}
+				sx={{ width: '100%', marginBottom: 2 }}
+			/>
+			<TextField
+				variant={'outlined'}
+				label={'Email-Address'}
+				sx={{ width: '100%', marginBottom: 2 }}
+			/>
+			<TextField
+				type={'password'}
+				variant={'outlined'}
+				label={'Password'}
+				sx={{ width: '100%', marginBottom: 2 }}
+			/>
+			<Button variant={'contained'} sx={{ marginBottom: 1 }}>
+				Login
+			</Button>
+			<Button variant={'text'} onClick={() => setMode(Modes.LOGIN)}>
+				already have an account
+			</Button>
+		</Grid>
+	);
+
 	return (
 		<Box
 			sx={{
@@ -9,13 +81,13 @@ export function LoginBox() {
 			}}
 		>
 			<Paper
-				elevation={1}
+				elevation={4}
 				sx={{
-					padding: 1,
+					padding: 2,
 				}}
 			>
-				<Typography variant={'h5'}>Login</Typography>
-				<TextField variant={'outlined'} label={'Username'} />
+				{mode === Modes.LOGIN && Login}
+				{mode === Modes.REGISTER && Register}
 			</Paper>
 		</Box>
 	);
